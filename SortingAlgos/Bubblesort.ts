67f401a830fa772d74187810d665682f89699123
Roger Satsi <rdsatsi001@st.ug.edu.gh>
2023-02-20 21:25:56 +0000

    quicksort partition function

--- SortingAlgos/Bubblesort.ts
+++ SortingAlgos/Bubblesort.ts
@@ -5,7 +5,9 @@ const bubbleSort = (nums: Array<number>) : Array<number> => {
     for (let i = 0; i < nums.length - 1; i++) {
 
         for (let j = 0; j < nums.length; j++) {
+
             if (nums[j] > nums[j + 1]) {
+
                 let temp = nums[j]
                 nums[j] = nums[j + 1]
                 nums[j + 1] = temp
